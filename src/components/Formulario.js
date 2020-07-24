5ef1d09e2f140299cbf2a78998a0311d191c7a49
mromero <mgr_89@hotmail.com>
2020-07-24 12:10:55 -0300

    errorFormulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,20 +1,116 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from '@emotion/styled';
+
+
+const Campo= styled.div`
+display:flex;
+margin-bottom:1rem;
+align-items: center;
+`;
+
+const Label=styled.label`
+flex: 0 0 100px;    
+`;
+const Select=styled.select`
+display:block;
+width:100%;
+padding:1rem;
+border:1px solid #e1e1e1;
+-webkit-appearance:none;
+
+`;
+
+const InputRadio=styled.input`
+margin:0 1rem;
+`;
+
+const Boton = styled.button`
+background-color:#00838F;
+font-size:16px;
+width:100%;
+padding:1rem;
+color:#fff;
+text-transform: uppercase;
+font-weight: bold;
+border:none;
+transition:background-color .3s ease;
+margin-top: 2rem;
+&:hover{
+    background-color:#26c6da;
+    cursor:pointer;
+}
+`;
+
+const Error=styled.div`
+background-color:red;
+color:white;
+padding:1rem;
+width:100%;
+text-align:center;
+margin-bottom:3px;
+`;
+
+
 
 const Formulario = () => {
+    const [datos, guardarDatos]=useState({
+        marca:'',
+        year:'',
+        plan:''
+    });
+
+    const [error, guardarError]=useState(false);
+
+    //extraer los valores del state
+    const {marca, year, plan}=datos;
+
+    //leer los datos del formulario y pasarlos al state
+
+    const obtenerInformacion = (e)=>{
+        guardarDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        })
+    }
+    //cuando el usuario manda submit
+    const cotizarSeguro = e=>{
+        // console.log('hola')
+        e.preventDefault();
+        if(marca.trim()===''|| year.trim()==='' ||plan.trim()===''){
+          guardarError(true);
+          return;  
+        }
+        guardarError(false);
+
+    }
+
+
+
     return (
-        <form>
-            <div>
-                <label>Marca</label>
-                <select>
+        <form
+        onSubmit={cotizarSeguro}
+        >
+            {error? <Error>Completar Campos</Error>:null}
+            <Campo>
+                <Label>Marca</Label>
+                <Select
+                name='marca'
+                value={marca}
+                onChange={obtenerInformacion}
+                >
                     <option value=''>-- Seleccione --</option>
                     <option value='americano'> americano </option>
                     <option value='europeo'> europeo </option>
                     <option value='asiatico'> asiatico </option>
-                </select>
-            </div>
-            <div>
-                <label>Año</label>
-                <select>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Año</Label>
+                <Select
+                name='year'
+                value={year}
+                onChange={obtenerInformacion}
+                >
                     <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -26,25 +122,29 @@ const Formulario = () => {
                     <option value="2014">2014</option>
                     <option value="2013">2013</option>
                     <option value="2012">2012</option>
-                </select>
-            </div>
-            <div>
-                <label>Plan</label>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Plan</Label>
                 
-                <input
+                <InputRadio
                 type='radio'
                 name='plan'
                 value='basico'
+                checked={plan==='basico'}
+                onChange={obtenerInformacion}
                 />Basico
 
-                <input
+                <InputRadio
                 type='radio'
                 name='plan'
                 value='completo'
+                checked={plan==='completo'}
+                onChange={obtenerInformacion}
                 />Completo
 
-            </div>
-            <button type='button'>Cotizar</button>
+            </Campo>
+            <Boton type='submit'>Cotizar</Boton>
         </form>
     );
 }
